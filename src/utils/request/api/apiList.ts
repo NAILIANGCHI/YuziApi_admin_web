4848fc05bb1593a98264c375c174018b4e685901
ShenZhaoYu <2905821826@qq.com>
2024-06-14 00:38:02 +0800

    feat: 菜单添加

--- src/utils/request/api/apiList.ts
+++ src/utils/request/api/apiList.ts
@@ -12,9 +12,20 @@ export const userInfo = async () => {
 
 
 // 用户登录接口
-export const login = async (data) => {
+export const login = async (data: any) => {
     return await request({
         url: '/user/login',
+        method: 'post',
+        data: {
+            ...data
+        }
+    });
+}
+
+// 新增菜单接口
+export const addMenu = async (data: any) => {
+    return await request({
+        url: '/menu/add',
         method: 'post',
         data: {
             ...data
