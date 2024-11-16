cb62253c5b551245ec2cede949da2ee8da42705f
ShenZhaoYu <2905821826@qq.com>
2024-11-16 20:17:22 +0800

    perf: 优化登录页面

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,10 +1,11 @@
 import { useNavigate } from 'react-router-dom';
-import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import { Button, Form, Input, message } from 'antd';
-import ParticlesBg from 'particles-bg';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import { login } from '@/utils/request/api/apiList';
-import Foot from '@/compontes/Foot/Foot';
 import { AxiosResponse, AxiosError } from 'axios';
+import { startLoading, stopLoading } from '@/store/loadingSlice';
+import "@/utils/style/login.css";
+import {useDispatch} from "react-redux";
 
 interface LoginFormValues {
   username: string;
@@ -25,120 +26,106 @@ interface DataVerify {
   };
 }
 
-interface ErrorData {
-  message: string;
-}
-
 const Login = () => {
   const [messageApi, contextHolder] = message.useMessage();
   const navigate = useNavigate();
+  const dispatch = useDispatch()
 
-  const success = (message: string) => {
-    messageApi.open({
-      type: 'success',
-      content: message,
-    });
+  const handleSuccess = (msg: string) => {
+    messageApi.success(msg);
   };
 
-  const errorWindows = (message: string) => {
-    messageApi.open({
-      type: 'error',
-      content: message,
-    });
+  const handleError = (msg: string) => {
+    messageApi.error(msg);
   };
 
-  // 保存登录信息
-  const saveData = () => {
+  const saveDataAndRedirect = () => {
     navigate('/');
   };
 
   const onFinish = async (values: LoginFormValues) => {
+    dispatch(startLoading())
     const data: LoginData = {
       email: values.username,
       password: values.password,
-      code: ''
+      code: "",
     };
-
     try {
       const response: AxiosResponse<DataVerify> = await login(data);
-      const responseData: DataVerify = response.data;
+      const { code, message: responseMessage, data: responseData } = response.data;
 
-      if (responseData.code !== '200') {
-        console.log(responseData.code)
-        errorWindows(responseData.message);
+      if (code !== '200') {
+        dispatch(stopLoading())
+        handleError(responseMessage);
       } else {
-        success('登录成功');
-        localStorage.setItem('token', responseData.data.token);
-        setTimeout(saveData, 2000);
+        dispatch(stopLoading())
+        handleSuccess('登录成功');
+        localStorage.setItem('token', responseData.token);
+        setTimeout(saveDataAndRedirect, 2000);
       }
     } catch (error) {
       if (error instanceof AxiosError) {
-        const errorData: ErrorData = error.response?.data || { message: error.message };
-        errorWindows(errorData.message);
+        dispatch(stopLoading())
+        const errorMsg = error.response?.data?.message || error.message;
+        handleError(errorMsg);
       } else {
-        errorWindows('An unexpected error occurred');
+        dispatch(stopLoading())
+        handleError('发生未知错误');
       }
     }
   };
 
   return (
-    <>
-      {contextHolder}
-      {/* 动态背景 */}
-      <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', zIndex: -1 }}>
-        <ParticlesBg type="circle" bg={true} />
-      </div>
-      <div style={{ width: "100%", height: "100vh", display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <Form style={{ width: "300px", paddingTop: "20px" }}
-          name="normal_login"
-          className="login-form"
-          initialValues={{
-            remember: true,
-          }}
-          onFinish={onFinish}
-        >
-          <h1 style={{fontSize: '30px', fontWeight: 'bold'}}>登录</h1>
-          <Form.Item
-            name="username"
-            rules={[
-              {
-                required: true,
-                message: '请输入邮箱',
-              },
-            ]}
-          >
-            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="邮箱" />
-          </Form.Item>
-          <Form.Item
-            name="password"
-            rules={[
-              {
-                required: true,
-                message: '请输入密码',
-              },
-            ]}
-          >
-            <Input
-              prefix={<LockOutlined className="site-form-item-icon" />}
-              type="password"
-              placeholder="密码"
-            />
-          </Form.Item>
-
-          <Form.Item>
-            <Button type="primary" htmlType="submit" className="login-form-button">
-              登录
-            </Button>
-            <br />
-            <a style={{ display: 'flex', justifyContent: 'right' }} href="https:///xxx">现在去注册</a>
-            <hr />
-          </Form.Item>
-        </Form>
-      </div>
-      <div>
-          <Foot/>
+      <>
+        {contextHolder}
+        <div className="site">
+          <div className="container">
+            <div className="login">
+              <div className="hero">
+                <h1>🐟<br />鱼子Api管理后台系统</h1>
+                <p>如果你没有账号<br />可以<a href="#">点击这里</a>进行注册.</p>
+              </div>
+              <div className="main">
+                <div className="form-container">
+                  <h1 className={"title"}>登录</h1>
+                  <Form
+                      name="login"
+                      onFinish={onFinish}
+                      autoComplete="off"
+                      layout="vertical"
+                  >
+                    <div className="form-fields">
+                      <Form.Item
+                          name="username"
+                          rules={[{ required: true, message: '请输入邮箱!' }]}
+                      >
+                        <Input
+                            placeholder="邮箱"
+                            prefix={<UserOutlined />}
+                        />
+                      </Form.Item>
+                      <Form.Item
+                          name="password"
+                          rules={[{ required: true, message: '请输入密码!' }]}
+                      >
+                        <Input.Password
+                            placeholder="密码"
+                            prefix={<LockOutlined />}
+                        />
+                      </Form.Item>
+                    </div>
+                    <Form.Item>
+                      <Button type="primary" htmlType="submit" block>
+                        登录
+                      </Button>
+                    </Form.Item>
+                  </Form>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    </>
+      </>
   );
 };
 
